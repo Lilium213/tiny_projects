32ad6fbd15f051bd8d2acba102e3a2367c8d6597
Alexander Bulychev <alexvc213@gmail.com>
2017-10-08 19:51:45 +0200

    Changed js

--- capture.js
+++ capture.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var errorElement = document.querySelector('#errorMsg');
 var video = document.querySelector('video');
 
 navigator.mediaDevices.getUserMedia({
   audio: false,
   video: true
 }).then(function(stream){
+    window.stream=stream;
     video.srcObject = stream;
 }).catch(function(err){
     console.log("An error has occured " + err);
