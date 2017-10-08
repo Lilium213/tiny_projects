6d111185f29072a22536890f13bcf1d0f35615d0
Alexander Bulychev <alexvc213@gmail.com>
2017-10-08 19:39:11 +0200

    Changed js and html

--- capture.js
+++ capture.js
@@ -1,54 +1,43 @@
-(function(){
-  var width=320;
-  var height=0;
+'use strict';
 
-  var streaming=false;
-  var video = null;
-  var canvas = null;
+var errorElement = document.querySelector('#errorMsg');
+var video = document.querySelector('video');
 
-  function startup(){
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+  audio: false,
+  video: true
+};
 
-    navigator.getMedia(
-      {
-        video: true,
-        audio: false
-      },
-      function(stream) {
-        if (navigator.mozGetUserMedia) {
-          video.mozSrcObject = stream;
-        } else {
-          var vendorURL = window.URL || window.webkitURL;
-          video.src = vendorURL.createObjectURL(stream);
-        }
-        video.play();
-      },
-      function(err) {
-        console.log("An error occured! " + err);
-      }
-    );
+function handleSuccess(stream) {
+  var videoTracks = stream.getVideoTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log('Using video device: ' + videoTracks[0].label);
+  stream.oninactive = function() {
+    console.log('Stream inactive');
+  };
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
+}
 
-    video.addEventListener('canplay', function(ev){
-     if (!streaming) {
-       height = video.videoHeight / (video.videoWidth/width);
-
-       // Firefox currently has a bug where the height can't be read from
-       // the video, so we will make assumptions if this happens.
-
-       if (isNaN(height)) {
-         height = width / (4/3);
-       }
-
-       video.setAttribute('width', width);
-       video.setAttribute('height', height);
-       canvas.setAttribute('width', width);
-       canvas.setAttribute('height', height);
-       streaming = true;
-     }
-   }, false);
+function handleError(error) {
+  if (error.name === 'ConstraintNotSatisfiedError') {
+    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+        constraints.video.width.exact + ' px is not supported by your device.');
+  } else if (error.name === 'PermissionDeniedError') {
+    errorMsg('Permissions have not been granted to use your camera and ' +
+      'microphone, you need to allow the page access to your devices in ' +
+      'order for the demo to work.');
   }
-  window.addEventListener('load', startup, false);
+  errorMsg('getUserMedia error: ' + error.name, error);
+}
 
+function errorMsg(msg, error) {
+  errorElement.innerHTML += '<p>' + msg + '</p>';
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
 
-})
+navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
